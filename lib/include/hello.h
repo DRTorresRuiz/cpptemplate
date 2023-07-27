9f172f06c2eb1ecfea2d10b00288898a621b064a
Daniel R. Torres Ruiz <danielr.torresruiz@gmail.com>
2023-07-27 22:33:26 +0200

    ✨ library and executable example w CMake

---
+++ lib/include/hello.h
@@ -0,0 +1,6 @@
+/* Copyright 2023 @DRTorresRuiz */
+#pragma once
+
+#include <string>
+
+std::string greet(std::string name);
---
+++ main.cpp
@@ -0,0 +1,9 @@
+/* Copyright 2023 @DRTorresRuiz */
+#include <hello.h>
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << greet("Mars") << std::endl;
+  return 0;
+}
