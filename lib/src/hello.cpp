4aa9427658007faf37dbf78061abf9f71112b02f
Daniel R. Torres Ruiz <danielr.torresruiz@gmail.com>
2023-07-30 22:32:41 +0200

    ➕FetchContent example w `fmt`

--- lib/src/hello.cpp
+++ lib/src/hello.cpp
@@ -1,5 +1,4 @@
 /* Copyright 2023 @DRTorresRuiz */
 #include <hello.h>
-// #include <fmt/chrono.h>
 
 std::string greet(std::string name) { return "Hello " + name + "!"; }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 /* Copyright 2023 @DRTorresRuiz */
+#include <fmt/chrono.h>
+#include <fmt/color.h>
 #include <hello.h>
 
-#include <iostream>
-
 int main(int argc, char const *argv[]) {
-  std::cout << greet("Mars") << std::endl;
+  std::time_t t = std::time(nullptr);
+  fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+             "[{:%Y-%m-%d %H:%M:%S}]: ", fmt::localtime(t));
+  fmt::print("{}\n", greet("Mars"));
   return 0;
 }
